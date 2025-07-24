efc30d4eae54a44962e778829d377abbf06f5b56
venkat <venkat.tanneru92@gmail.com>
2025-07-24 18:38:47 +0530

    Day2 Practice Assessment code Commited

---
+++ Day2/Practice Assessment(D-2)/PracticeAssessment/PracticeAssessment/Code1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Student
+{
+    // Define properties
+    public string Name;
+    public int Age;
+    public string Grade;
+
+    // Define constructor
+    public Student(string name, int age, string grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+}
+
+internal class Code1
+{
+    static void Main(string[] args)
+    {
+        // Prompt the user to enter student details
+        Console.WriteLine("Enter student's name:");
+        string Name = Console.ReadLine();
+
+        Console.WriteLine("Enter student's age:");
+        int Age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter student's grade:");
+        string Grade = Console.ReadLine();
+
+        // Create an instance of the Student class
+        Student stud = new Student(Name, Age, Grade);
+
+        // Print student details
+        Console.WriteLine("Student Name: " + stud.Name);
+        Console.WriteLine("Student Age: " + stud.Age);
+        Console.WriteLine("Student Grade: " + stud.Grade);
+    }
+}
---
+++ Day2/Practice Assessment(D-2)/PracticeAssessment/PracticeAssessment/Code2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Book
+{
+    // Define properties
+    public string Title;
+    public string Author;
+    public int Year;
+
+    // Define constructor
+    public Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+}
+
+internal class Code2
+{
+    static void Main(string[] args)
+    {
+        // Prompt the user to enter book details
+        Console.WriteLine("Enter book's title:");
+        string Title = Console.ReadLine();
+
+        Console.WriteLine("Enter book's author:");
+        string Author = Console.ReadLine();
+
+        Console.WriteLine("Enter book's year:");
+        int Year = Convert.ToInt32(Console.ReadLine());
+
+        // Create an instance of the Book class
+        Book book = new Book(Title, Author, Year);
+
+        // Print book details
+        Console.WriteLine("Book Title: " + book.Title);
+        Console.WriteLine("Book Author: " + book.Author);
+        Console.WriteLine("Book Year: " + book.Year);
+    }
+}
